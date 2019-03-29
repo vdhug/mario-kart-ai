f3bf49adb8dc9287779c24d62f6a7d47f5a5e7ff
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-03-29 20:21:47 -0300

    Defining a  ROI (region of interest) in the processed image

--- main.py
+++ main.py
@@ -13,12 +13,24 @@ for i in list(range(4))[::-1]:
 
 last_time = time.time()
 
+def roi(img, vertices):
+    #blank mask:
+    mask = np.zeros_like(img)
+    # fill the mask
+    cv2.fillPoly(mask, vertices, 255)
+    # now only show the area that is the mask
+    masked = cv2.bitwise_and(img, mask)
+    return masked
+
 
 def process_image(original_image):
     # convert to gray
     processed_img = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
     # edge detection
     processed_img =  cv2.Canny(processed_img, threshold1 = 200, threshold2=300)
+    vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500],
+                         ], np.int32)
+    processed_img = roi(processed_img, [vertices])
     return processed_img
 
 
