8063db54b8557b5c427e278dceddbaa94350c06e
[Vitor Hugo] <silvalimavitorhugo@gmail.com>
2019-03-30 16:26:13 -0300

    Adding git ignore file

---
+++ balance_data.py
@@ -0,0 +1,12 @@
+# balance_data.py
+
+import numpy as np
+import pandas as pd
+from collections import Counter
+from random import shuffle
+
+train_data = np.load('training_data.npy')
+
+df = pd.DataFrame(train_data)
+print(df.head())
+print(Counter(df[1].apply(str)))
